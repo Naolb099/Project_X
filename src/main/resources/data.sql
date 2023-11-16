4ee40bc81fa74140c185983b13aea832b71a5f38
neo <ykassa47@gmail.com>
2023-11-15 19:49:53 -0600

    database schema up and running

---
+++ src/main/resources/data.sql
@@ -0,0 +1,5 @@
+INSERT INTO student (id, name, passport_number) VALUES (1, 'John Doe', 'A12345678');
+INSERT INTO student (id, name, passport_number) VALUES (2, 'Emily Smith', 'B23456789');
+INSERT INTO student (id, name, passport_number) VALUES (3, 'Michael Johnson', 'C34567890');
+INSERT INTO student (id, name, passport_number) VALUES (4, 'Sara Lee', 'D45678901');
+INSERT INTO student (id, name, passport_number) VALUES (5, 'David Brown', 'E56789012');

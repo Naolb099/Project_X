b9695a746a77ef2bcf72283d0f2914bb0272c1c4
Gurmessa <naoljemal99@gmail.com>
2023-11-16 21:56:00 -0600

    model classes added

---
+++ src/main/java/model/Post.java
@@ -0,0 +1,12 @@
+package model;
+import java.util.Date;
+
+public class Post {
+    private Long postId;
+    private Long userId;
+    private String title;
+    private String content;
+    private Date postDate;
+    // Constructors, getters, and setters
+}
+
---
+++ src/main/java/model/PostTracker.java
@@ -0,0 +1,11 @@
+package model;
+
+public class PostTracker {
+    private Long postTrackerId;
+    private Long postId;
+    private Integer upvoteCount;
+    private Integer downvoteCount;
+
+    // Constructors, getters, and setters
+}
+

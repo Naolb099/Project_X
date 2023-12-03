da8ff24e21b7b0dde38fc6aff37b0d7256a07869
Dylan Hilger <Dylan.Hilger@trojans.dsu.edu>
2023-12-03 12:28:28 -0600

    Populate current user info (placeholder)

--- src/main/java/com/projectX_F23/ProjectX/Project_X/controller/UserSettingsController.java
+++ src/main/java/com/projectX_F23/ProjectX/Project_X/controller/UserSettingsController.java
@@ -1,13 +1,34 @@
 package com.projectX_F23.ProjectX.Project_X.controller;
 
+import com.projectX_F23.ProjectX.Project_X.repository.UserRepository;
+import com.projectX_F23.ProjectX.Project_X.model.User;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
 
 @Controller
 public class UserSettingsController {
+    @Autowired
+    public UserSettingsController(UserRepository userRepository) {
+    }
+
     @GetMapping("/usersettings")
-    public String showUserSettings()
-    {
+    public String showUserSettings(User user, Model model) {
+//        User currentUser = userRepository.findByUsername(user.getUsername());
+        User currentUser = getCurrentUser();
+        model.addAttribute("user", currentUser);
         return "userSettings";
+    }
+    private User getCurrentUser() {
+        //This is just a placeholder until findByUsername works
+        User user = new User();
+        user.setUsername("dylanleehilger");
+        user.setEmail("dylan.hilger@trojans.dsu.edu");
+        user.setPassword("Password1!");
+        return user;
     }
 }

ee81059740f5de925b3ce80e9798074f7a509cbc
Gurmessa <naoljemal99@gmail.com>
2023-12-03 23:46:40 -0600

    post model updated

--- src/main/java/com/projectX_F23/ProjectX/Project_X/model/Post.java
+++ src/main/java/com/projectX_F23/ProjectX/Project_X/model/Post.java
@@ -1,12 +1,26 @@
 package com.projectX_F23.ProjectX.Project_X.model;
-import java.util.Date;
+
+import java.time.LocalDateTime;
 
 public class Post {
     private Long postId;
     private Long userId;
     private String title;
     private String content;
-    private Date postDate;
-    // Constructors, getters, and setters
-}
+    private LocalDateTime postDate;
 
+    // Default constructor
+    public Post() {
+    }
+
+    // Constructor with fields
+    public Post(Long userId, String title, String content, LocalDateTime postDate) {
+        this.userId = userId;
+        this.title = title;
+        this.content = content;
+        this.postDate = postDate;
+    }
+
+    // Getters and setters
+    // ...
+}

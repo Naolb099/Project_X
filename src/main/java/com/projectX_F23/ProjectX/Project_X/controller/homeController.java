b5437565e97aad104150c17360163a14238f6d95
Wael Delessa <wael.delessa@trojans.dsu.edu>
2023-12-06 02:22:49 -0600

    Username displayable in homepage

--- src/main/java/com/projectX_F23/ProjectX/Project_X/controller/homeController.java
+++ src/main/java/com/projectX_F23/ProjectX/Project_X/controller/homeController.java
@@ -2,6 +2,7 @@ package com.projectX_F23.ProjectX.Project_X.controller;
 
 import com.projectX_F23.ProjectX.Project_X.model.User;
 import com.projectX_F23.ProjectX.Project_X.repository.UserRepository;
+import jakarta.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -9,11 +10,25 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
+
+
 @Controller
 public class homeController {
+
+    @Autowired
+    private UserRepository userRepository;
+
     @GetMapping("/home")
-    public String home()
-    {
+    public String home(Model model, HttpSession session) {
+        // Retrieve the logged-in user from the session
+        User loggedInUser = (User) session.getAttribute("loggedInUser");
+
+        // Check if the user is logged in
+        if (loggedInUser != null) {
+            // Add the user information to the model
+            model.addAttribute("loggedInUser", loggedInUser);
+        }
+
         return "home";
     }
 }

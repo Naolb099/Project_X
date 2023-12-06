28c89c96b5d90eff35fc8fda60dcfd75a91fcb85
Dylan Hilger <Dylan.Hilger@trojans.dsu.edu>
2023-12-06 17:21:30 -0600

    Cleaned up and formatted page to match login/signup pages

--- src/main/java/com/projectX_F23/ProjectX/Project_X/controller/UserSettingsController.java
+++ src/main/java/com/projectX_F23/ProjectX/Project_X/controller/UserSettingsController.java
@@ -5,7 +5,6 @@ import com.projectX_F23.ProjectX.Project_X.model.User;
 import jakarta.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;

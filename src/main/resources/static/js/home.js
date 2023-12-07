939f040d6f9934d5727db363968d7d0df4351505
neo <ykassa47@gmail.com>
2023-12-06 20:56:27 -0600

    minor fixes

--- src/main/resources/static/js/home.js
+++ src/main/resources/static/js/home.js
@@ -1,6 +1,6 @@
 function show_hide() {
     var click = document.getElementById("list-items");
-    if (click.style.display === "none") {
+    if (click.style.display === "" || click.style.display === "none") {
         click.style.display = "block";
     } else {
         click.style.display = "none";
